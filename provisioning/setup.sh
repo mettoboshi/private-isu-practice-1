2daf5084d5911fc4cbe541e4750c22553934a9a1
mettoboshi <mettoboshi.1108@gmail.com>
2023-07-27 16:07:51 +0900

    toolsディレクトリがない場合作成するようにした

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-# curl -sL -o ../tools/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-x86_64-linux
-curl -sL -o ../tools/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-aarch64-linux
+# toolsディレクトリを作成
+tools_dir="../../tools"
+mkdir -p "$tools_dir"
 
-chmod +x ../tools/mitamae
-../tools/mitamae version
+# mitamae
+# curl -sL -o "$tools_dir"/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-x86_64-linux
+curl -sL -o "$tools_dir"/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-aarch64-linux
+chmod +x "$tools_dir"/mitamae
+"$tools_dir"/mitamae version
