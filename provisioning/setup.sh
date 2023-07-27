b5072faa581989d4db87d017f9f0a2cbda188655
mettoboshi <mettoboshi.1108@gmail.com>
2023-07-27 14:29:05 +0900

    ダウンロードするファイルを変更

--- provisioning/setup.sh
+++ provisioning/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # curl -sL -o ../tools/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-x86_64-linux
-curl -sL -o ../tools/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-aarch64-linux.tar.gz
+curl -sL -o ../tools/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/v1.14.0/mitamae-aarch64-linux
 
 chmod +x ../tools/mitamae
 ../tools/mitamae version
