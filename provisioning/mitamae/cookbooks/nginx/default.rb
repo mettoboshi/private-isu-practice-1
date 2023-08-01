b8ae096b3f4d7cdda847a1c093dc53d7f614539e
YASUO FUJITA <yasuofujita@YASUOnoMac-Studio.local>
2023-08-01 15:06:26 +0900

    ruby->phpへの変更するレシピを作成

---
+++ provisioning/mitamae/cookbooks/nginx/default.rb
@@ -0,0 +1,10 @@
+# isucon.confのシンボリックリンクを削除
+file "/etc/nginx/sites-enabled/isucon.conf" do
+  action :delete
+end
+
+# isucon-php.confのシンボリックリンクを作成
+link "/etc/nginx/sites-available/isucon-php.conf" do
+  to "/config/sites-available/isucon-php.conf"
+  action :create
+end
