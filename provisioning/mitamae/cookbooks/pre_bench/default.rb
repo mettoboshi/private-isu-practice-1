764a19521e5ee5b8226234a3d49e1050dc5bf1b1
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-03 16:07:14 +0900

    ベンチマーカーを回す前にログをローテートするレシピを作成

---
+++ provisioning/mitamae/cookbooks/pre_bench/default.rb
@@ -0,0 +1,25 @@
+MYSQL_LOG = "/var/log/mysql/slow-query.log"
+NGINX_LOG = "/var/log/nginx/access.log"
+current_time = `date "+%Y%m%d_%H%M%S"`.strip
+
+# MySQLログファイルの移動
+execute "Move MySQL slow query log" do
+  command "sudo mv #{MYSQL_LOG} #{MYSQL_LOG}.#{current_time}"
+  only_if "test -f #{MYSQL_LOG}"
+end
+
+# NGINXログファイルの移動
+execute "Move NGINX access log" do
+  command "sudo mv #{NGINX_LOG} #{NGINX_LOG}.#{current_time}"
+  only_if "test -f #{NGINX_LOG}"
+end
+
+# MySQLの再起動
+service "mysql" do
+  action :restart
+end
+
+# NGINXの再起動
+service "nginx" do
+  action :restart
+end
