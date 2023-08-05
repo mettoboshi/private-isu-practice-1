a72846a9f98b676df54418872faea210ce933b4e
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-06 00:01:58 +0900

    mysqlの構造のみdump生成できるレシピを追加

---
+++ provisioning/mitamae/cookbooks/mysql/dump_structure.rb
@@ -0,0 +1,15 @@
+MYSQL_USER = 'isuconp'
+MYSQL_PASSWORD = 'isuconp'
+MYSQL_DATABASE = 'isuconp'
+DUMP_DIR = "/home/isucon/tmp"
+DUMP_FILE = "#{DUMP_DIR}/dump.sql"
+
+# ディレクトリの生成
+directory DUMP_DIR do
+  action :create
+end
+
+# dumpファイルの作成
+execute "Dump MySQL structure" do
+  command "mysqldump --user=#{MYSQL_USER} --password=#{MYSQL_PASSWORD} --no-data #{MYSQL_DATABASE} > #{DUMP_FILE}"
+end
