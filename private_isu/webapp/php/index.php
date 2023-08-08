fad224159a468daa7072a5c80e6dea245e625a8c
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-08 23:10:16 +0900

    SQLの微修正（posts追加。left joinをinner joinに変更

--- private_isu/webapp/php/index.php
+++ private_isu/webapp/php/index.php
@@ -297,7 +297,7 @@ $app->get('/', function (Request $request, Response $response) {
     $me = $this->get('helper')->get_session_user();
 
     $db = $this->get('db');
-    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` left join `users` on `posts`.`user_id` = `users`.`id` where `users`.`del_flg` = 0 ORDER BY `created_at` DESC LIMIT 20');
+    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` inner join `users` on `posts`.`user_id` = `users`.`id` where `users`.`del_flg` = 0 ORDER BY `posts`.`created_at` DESC LIMIT 20');
     $ps->execute();
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = $this->get('helper')->make_posts($results);
@@ -313,7 +313,7 @@ $app->get('/posts', function (Request $request, Response $response) {
     $params = $request->getQueryParams();
     $max_created_at = $params['max_created_at'] ?? null;
     $db = $this->get('db');
-    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` left join `users` on `posts`.`user_id` = `users`.`id` where `users`.`del_flg` = 0 and `posts`.`created_at` <= ? ORDER BY `created_at` DESC LIMIT 20');
+    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` INNER JOIN `users` ON `posts`.`user_id` = `users`.`id` WHERE `users`.`del_flg` = 0 AND `posts`.`created_at` <= ? ORDER BY `posts`.`created_at` DESC LIMIT 20');
     $ps->execute([$max_created_at === null ? null : $max_created_at]);
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = $this->get('helper')->make_posts($results);
@@ -493,7 +493,7 @@ $app->get('/@{account_name}', function (Request $request, Response $response, $a
         $response->getBody()->write('404');
         return $response->withStatus(404);
     }
-    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` left join `users` on `posts`.`user_id` = `users`.`id` where `users`.`del_flg` = 0 and `posts`.`user_id` = ? ORDER BY `created_at` DESC LIMIT 20');
+    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` inner join `users` on `posts`.`user_id` = `users`.`id` where `users`.`del_flg` = 0 and `posts`.`user_id` = ? ORDER BY `posts`.`created_at` DESC LIMIT 20');
     $ps->execute([$user['id']]);
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = $this->get('helper')->make_posts($results);
