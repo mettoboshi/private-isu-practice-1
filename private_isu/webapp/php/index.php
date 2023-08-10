d3b79e5375f5a50d9af0d1876663a99ae5dc2132
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-10 15:04:46 +0900

    SQLを複数行にした

--- private_isu/webapp/php/index.php
+++ private_isu/webapp/php/index.php
@@ -345,7 +345,21 @@ $app->get('/posts', function (Request $request, Response $response) {
     $params = $request->getQueryParams();
     $max_created_at = $params['max_created_at'] ?? null;
     $db = $this->get('db');
-    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` inner join `users` ON `posts`.`user_id` = `users`.`id` WHERE `users`.`del_flg` = 0 AND `posts`.`created_at` <= ? ORDER BY `posts`.`created_at` DESC LIMIT 20');
+    $ps = $db->prepare(
+        'SELECT ' .
+        '`posts`.`id`, ' .
+        '`posts`.`user_id`, ' .
+        '`posts`.`body`, ' .
+        '`posts`.`mime`, ' .
+        '`posts`.`created_at` ' .
+        'FROM `posts` ' .
+        'inner join `users` on ' .
+        '`posts`.`user_id` = `users`.`id` ' .
+        'where ' .
+        '`users`.`del_flg` = 0 ' .
+        'and `posts`.`created_at` <= ? ' .
+        'ORDER BY `posts`.`created_at` DESC ' .
+        'LIMIT 20');
     $ps->execute([$max_created_at === null ? null : $max_created_at]);
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = $this->get('helper')->make_posts($results);
@@ -525,7 +539,22 @@ $app->get('/@{account_name}', function (Request $request, Response $response, $a
         $response->getBody()->write('404');
         return $response->withStatus(404);
     }
-    $ps = $db->prepare('SELECT `posts`.`id`, `posts`.`user_id`, `posts`.`body`, `posts`.`mime`, `posts`.`created_at` FROM `posts` inner join `users` on `posts`.`user_id` = `users`.`id` where `users`.`del_flg` = 0 and `posts`.`user_id` = ? ORDER BY `posts`.`created_at` DESC LIMIT 20');
+    $ps = $db->prepare(
+        'SELECT ' .
+        '`posts`.`id`, ' .
+        '`posts`.`user_id`, ' .
+        '`posts`.`body`, ' .
+        '`posts`.`mime`, ' .
+        '`posts`.`created_at` ' .
+        'FROM `posts` ' .
+        'inner join `users` on ' .
+        '`posts`.`user_id` = `users`.`id` ' .
+        'where ' .
+        '`users`.`del_flg` = 0' .
+        'and `posts`.`user_id` = ?' .
+        'ORDER BY `posts`.`created_at` DESC ' .
+        'LIMIT 20');
+
     $ps->execute([$user['id']]);
     $results = $ps->fetchAll(PDO::FETCH_ASSOC);
     $posts = $this->get('helper')->make_posts($results);
