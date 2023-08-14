42bff67ad6c68358134f53a42d4a69f8f2645df3
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-15 00:10:16 +0900

    nginx関連のファイルを変更した時にバックアップを取得するようにした

--- provisioning/mitamae/cookbooks/nginx/default.rb
+++ provisioning/mitamae/cookbooks/nginx/default.rb
@@ -1,6 +1,21 @@
+# 現在の日時を取得
+current_time = Time.now.strftime("%Y%m%d%H%M%S")
+
 # isucon.confのシンボリックリンクを削除
 file "/etc/nginx/sites-enabled/isucon.conf" do
   action :delete
+end
+
+# isucon-php.confのシンボリックリンクを作成
+link "/etc/nginx/sites-enabled/isucon-php.conf" do
+  to ("/etc/nginx/sites-available/isucon-php.conf")
+  action :create
+end
+
+# nginx.confのバックアップ
+execute "backup nginx.conf" do
+  command "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.#{current_time}"
+  only_if "diff -q /etc/nginx/nginx.conf config/nginx.conf"
 end
 
 # nginx.confをコピー
@@ -11,16 +26,16 @@ remote_file "/etc/nginx/nginx.conf" do
   mode   "644"
 end
 
+# isucon-php.confのバックアップ
+execute "backup isucon-php.conf" do
+  command "mv /etc/nginx/sites-available/isucon-php.conf /etc/nginx/sites-available/isucon-php.conf.#{current_time}"
+  only_if "diff -q /etc/nginx/sites-available/isucon-php.conf config/sites-available/isucon-php.conf"
+end
+
 # isucon-php.confをコピー
 remote_file "/etc/nginx/sites-available/isucon-php.conf" do
   owner  "root"
   group  "root"
   source "config/sites-available/isucon-php.conf"
   mode   "644"
-end
-
-# isucon-php.confのシンボリックリンクを作成
-link "/etc/nginx/sites-enabled/isucon-php.conf" do
-  to ("/etc/nginx/sites-available/isucon-php.conf")
-  action :create
 end
