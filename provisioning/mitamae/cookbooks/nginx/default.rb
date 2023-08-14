338652e98bae461ff89248e6caf39285b30090ff
mettoboshi <mettoboshi.1108@gmail.com>
2023-08-15 00:25:02 +0900

    cookbookのディレクトリから相対パスでdiffをとるようにした

--- provisioning/mitamae/cookbooks/nginx/default.rb
+++ provisioning/mitamae/cookbooks/nginx/default.rb
@@ -1,6 +1,9 @@
 # 現在の日時を取得
 current_time = Time.now.strftime("%Y%m%d%H%M%S")
 
+# カレントディレクトリを取得
+current_dir = File.dirname(__FILE__)
+
 # isucon.confのシンボリックリンクを削除
 file "/etc/nginx/sites-enabled/isucon.conf" do
   action :delete
@@ -15,7 +18,7 @@ end
 # nginx.confのバックアップ
 execute "backup nginx.conf" do
   command "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.#{current_time}"
-  only_if "diff -q /etc/nginx/nginx.conf config/nginx.conf"
+  only_if "diff -q /etc/nginx/nginx.conf #{current_dir}/config/nginx.conf"
 end
 
 # nginx.confをコピー
@@ -29,7 +32,7 @@ end
 # isucon-php.confのバックアップ
 execute "backup isucon-php.conf" do
   command "mv /etc/nginx/sites-available/isucon-php.conf /etc/nginx/sites-available/isucon-php.conf.#{current_time}"
-  only_if "diff -q /etc/nginx/sites-available/isucon-php.conf config/sites-available/isucon-php.conf"
+  only_if "diff -q /etc/nginx/sites-available/isucon-php.conf #{current_dir}/config/sites-available/isucon-php.conf"
 end
 
 # isucon-php.confをコピー
